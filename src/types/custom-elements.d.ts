25106c248da785e45aa31a4e8540e9d4a124dc0e
mccrann <110939204+mccrann@users.noreply.github.com>
2026-07-21 14:11:20 +0000

    setup for shared back button

---
+++ src/types/custom-elements.d.ts
@@ -0,0 +1,8 @@
+declare namespace JSX {
+  interface IntrinsicElements {
+    "portfolio-return": React.DetailedHTMLProps<
+      React.HTMLAttributes<HTMLElement>,
+      HTMLElement
+    >
+  }
+}
